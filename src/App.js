e4304cd37ae5a0271f7028b7effdca2d4d506e94
JoseAzmitia <azmitiajc@gmail.com>
2023-08-04 20:33:19 -0600

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ArticlesListPage from "./pages/ArticlesListPage";
@@ -6,10 +8,19 @@ import ArticlePage from "./pages/ArticlePage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Awesome Blog</h1>
-      <div id="page-body">Welcome to my blog!</div>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/articles" element={<ArticlesListPage />} />
+            <Route path="/articles/:articleId" element={<ArticlePage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
