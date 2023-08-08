adb9a6e1adc978166122eb7546facd5a038f3d28
JoseAzmitia <azmitiajc@gmail.com>
2023-08-07 22:19:09 -0600

    Added firebase and LoginPage

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZBxXFV-5fKTes0fwCnPI_7frGVti8wOs",
+  authDomain: "my-react-blog-b157c.firebaseapp.com",
+  projectId: "my-react-blog-b157c",
+  storageBucket: "my-react-blog-b157c.appspot.com",
+  messagingSenderId: "564840144285",
+  appId: "1:564840144285:web:3ceba72910cfe0b2796374",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
