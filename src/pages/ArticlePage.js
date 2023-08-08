d88d7a845a7614adb4b2f55a529a59f7a282fb88
JoseAzmitia <azmitiajc@gmail.com>
2023-08-07 18:21:20 -0600

    Added AddCommentForm component

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
 import CommentsList from "../components/CommentsList";
+import AddCommentForm from "../components/AddCommentForm";
 
 const ArticlePage = () => {
   const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
@@ -29,16 +30,21 @@ const ArticlePage = () => {
   if (!article) {
     return <NotFoundPage />;
   }
+
   return (
     <>
       <h1>{article.title}</h1>
-      <button className="upvotes-section" onClick={addUpvote}>
-        Upvote
-      </button>
-      <p>This article has {articleInfo.upvotes} upvotes(s)</p>
+      <div className="upvotes-section">
+        <button onClick={addUpvote}>Upvote</button>
+        <p>This article has {articleInfo.upvotes} upvote(s)</p>
+      </div>
       {article.content.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
       ))}
+      <AddCommentForm
+        articleName={articleId}
+        onArticleUpdated={(updatedArticle) => setArticleInfo(updatedArticle)}
+      />
       <CommentsList comments={articleInfo.comments} />
     </>
   );
