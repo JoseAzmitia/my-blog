edf1495f19cb19661da7bb1aba6c086e427f3ab7
JoseAzmitia <azmitiajc@gmail.com>
2023-08-08 21:56:33 -0600

    Added headers

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -15,17 +15,27 @@ const ArticlePage = () => {
 
   useEffect(() => {
     const loadArticleInfo = async () => {
-      const response = await axios.get(`/api/articles/${articleId}`);
+      const token = user && (await user.getIdToken());
+      const headers = token ? { authoken: token } : {};
+      const response = await axios.get(`/api/articles/${articleId}`, {
+        headers,
+      });
       const newArticleInfo = response.data;
       setArticleInfo(newArticleInfo);
     };
     loadArticleInfo();
-  }, [articleId]);
+  }, [articleId, user]);
 
   const article = articles.find((article) => article.name === articleId);
 
   const addUpvote = async () => {
-    const response = await axios.put(`/api/articles/${articleId}/upvote`);
+    const token = user && (await user.getIdToken());
+    const headers = token ? { authoken: token } : {};
+    const response = await axios.put(
+      `/api/articles/${articleId}/upvote`,
+      null,
+      { headers }
+    );
     const updateArticle = response.data;
     setArticleInfo(updateArticle);
   };
